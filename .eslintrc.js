4ec185f5887b7cb52f78658341585d6708e02b6a
Martynov <kseexy@mail.ru>
2022-06-19 16:33:18 +0300

    configure webpack config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:react/recommended',
-    'google',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-  ],
-  'rules': {
-    'react/display-name': 'off',
-    'react/react-in-jsx-scope': 'off',
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/display-name': 'off'
   },
 };
