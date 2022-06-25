689534401d6e75dea1c44d2e3dcbc07345522897
Martynov <kseexy@mail.ru>
2022-06-25 17:47:41 +0300

    add alias to eslint

--- src/_api/_http/api.ts
+++ src/_api/_http/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.VITE_APP_API_URL,
+    baseURL: process.env.APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
